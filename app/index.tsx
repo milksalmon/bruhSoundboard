48f0008e974099dd4750a9dfd69042b257320ca2
milksalmon <zhlam1202@gmail.com>
2025-04-09 00:15:20 +0800

    yeah the stuff

---
+++ app/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import {Text, View, TouchableWithoutFeedback, Image } from "react-native";
+import {Audio} from 'expo-av';
+
+
+
+export default function Index() {
+  const [sound, setSound] = useState<Audio.Sound | undefined>(undefined);
+  const [bruhImage, setBruhImage] = useState(require('../assets/images/Button.png'));
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sounds/bruh_sound_effect.mp3'));
+    setSound(sound);
+
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+
+
+  function Pressed () {
+    setBruhImage(require('../assets/images/Button_Pressed.png'))
+  
+
+  }
+
+  function pressRelease () {
+    playSound();
+    setBruhImage(require('../assets/images/Button.png'));
+
+  }
+
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <TouchableWithoutFeedback onPressIn={Pressed} onPressOut={pressRelease}><Image source={bruhImage}/></TouchableWithoutFeedback>
+    </View>
+  );
+}
