1b69dad7f521ac00c9ae2ba8c12f05e66a808f7f
milksalmon <zhlam1202@gmail.com>
2025-04-09 01:35:14 +0800

    Added tally to play 'let me know' every 50 presses.

--- app/index.tsx
+++ app/index.tsx
@@ -5,37 +5,61 @@ import {Audio} from 'expo-av';
 
 
 export default function Index() {
-  const [sound, setSound] = useState<Audio.Sound | undefined>(undefined);
+  const [bruhSound, setBruhSound] = useState<Audio.Sound | undefined>(undefined);
+  const [LMKSound, setLMKSound] = useState<Audio.Sound | undefined>(undefined);
   const [bruhImage, setBruhImage] = useState(require('../assets/images/Button.png'));
+  const [pressCount, setPressCount] = useState(0);
 
-  async function playSound() {
+  //(--- Sound manager
+  async function playBruhSound() {
     const { sound } = await Audio.Sound.createAsync( require('../assets/sounds/bruh_sound_effect.mp3'));
-    setSound(sound);
+    setBruhSound(sound);
 
     await sound.playAsync();
   }
 
   useEffect(() => {
-    return sound
+    return bruhSound
       ? () => {
           console.log('Unloading Sound');
-          sound.unloadAsync();
+          bruhSound.unloadAsync();
         }
       : undefined;
-  }, [sound]);
+  }, [bruhSound]);
 
+  async function playLMK() {
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sounds/let_me_know.mp3'));
+    setLMKSound(sound);
 
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    return LMKSound
+      ? () => {
+          console.log('Unloading Sound');
+          LMKSound.unloadAsync();
+        }
+      : undefined;
+  }, [LMKSound]);
+  //---)
+
+  
 
   function Pressed () {
     setBruhImage(require('../assets/images/Button_Pressed.png'))
-  
-
   }
 
   function pressRelease () {
-    playSound();
     setBruhImage(require('../assets/images/Button.png'));
-
+    setPressCount(prev => {
+      console.log("This guy reached " + prev);
+      if (prev % 50 === 0) {
+        playLMK();
+      } else {
+        playBruhSound();
+      }
+      return prev + 1});
   }
 
 
